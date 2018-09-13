76c2856ab6e43a10b70049ff2763ce367e5dffe4
Kazumichi Soeno <Kazumichi.Soeno@nagasebros.com>
2018-09-13 13:08:05 +0900

    vs code github pullrequest  test

--- a.pl
+++ a.pl
@@ -4,24 +4,43 @@ use utf8;
 use strict;
 use warnings;
 
-use IO::File;
+use Encode::Locale;
+use Encode qw( find_encoding );
+my $fs = find_encoding('locale_fs');
 
-my $file = "foo.txt";
+binmode *STDOUT => ':encoding(console_out)';
+
+my $file = $fs->encode("file.txt");
+
+use IO::File;
 
 # write.
 my $wfh = IO::File->new( $file, "w" );
 if ( defined $wfh ) {
+  binmode $wfh => ':crlf :encoding(cp932)';
   foreach my $i ( 0 .. 9 ) {
-    $wfh->print( "test ", $i, "\n" );
+    $wfh->print( "test 日本語 ", $i, "\n" );
   }
   $wfh->close();
 }
 
+my $co = find_encoding('console_out');
+
 # read.
 my $rfh = IO::File->new( $file, "r" );
 if ( defined $rfh ) {
+  binmode $rfh => ':crlf :encoding(cp932)';
   while ( my $in = <$rfh> ) {
     print $in;
   }
   undef $rfh;
 }
+
+# or...
+use autodie;
+use open IN => ':encoding(cp932)';
+open my $rfh2, '<', $file;
+while (my $in = <$rfh2>) {
+  print $in;
+}
+close $rfh2;
